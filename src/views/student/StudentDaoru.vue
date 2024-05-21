19eb30b54c82fefa02228b9d02e836dad4dab4a6
YJett <2212952919@qq.com>
2024-05-21 17:23:01 +0800

    修改选择文件报错的问题

--- src/views/student/StudentDaoru.vue
+++ src/views/student/StudentDaoru.vue
@@ -73,7 +73,7 @@ export default {
             courseUploadData: {},
             selectedStudentFileName: '',
             selectedStudentFile: null,
-            studentUploadUrl: '',
+            studentUploadUrl: 'import/studentInfoData',
             studentUploadData: {},
             schools: [],
         };

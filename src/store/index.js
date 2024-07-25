7f3a88a0622573322b80ccd75c7f93f31df1e510
YJett <2212952919@qq.com>
2024-07-25 14:05:24 +0800

    问题修改

--- src/store/index.js
+++ src/store/index.js
@@ -74,6 +74,12 @@ const store = new Vuex.Store({
                 text: '能力知识匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
             },
             {
+                name: 'radar', // 要跳转的路由名称 不是路径
+                size: 18, // icon大小
+                type: 'ios-paper', // icon类型
+                text: '学生画像', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+            },
+            {
                 name: 'jobportray', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-paper', // icon类型

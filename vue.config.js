906cbe6dd3476f4fc6939e1177db66bd1b4015b9
LuLi <3048715073@qq.com>
2025-05-12 21:11:22 +0800

    nodeserver转发

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,22 @@ module.exports = {
                 pathRewrite: {
                   ["^" + '/kg-api']: ""
                 }
-              }
+            },
+            // 添加Flask后端代理配置
+            '/flask-api': {
+                target: 'http://202.120.84.249:5000', // 服务器地址
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/flask-api': '',
+                },
+                proxyTimeout: 300000, // 5分钟超时
+                onProxyReq: (proxyReq, req, res) => {
+                    console.log(`[${new Date().toISOString()}] 转发Flask请求: ${req.method} ${req.url}`);
+                },
+                onProxyRes: (proxyRes, req, res) => {
+                    console.log(`[${new Date().toISOString()}] 收到Flask响应: ${proxyRes.statusCode} ${req.url}`);
+                }
+            }
         },
     },
     configureWebpack: {

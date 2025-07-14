9981351cfc3ffbf57356cce10146c88b883b3f96
littleli <2498653944@qqom>
2025-07-14 22:04:35 +0800

    change menu

--- src/store/index.js
+++ src/store/index.js
@@ -50,16 +50,12 @@ const store = new Vuex.Store({
                         type: 'ios-grid',
                         name: 'labilitydaoru',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     },
                     {
                         size: 18, // icon大小
                         type: 'ios-grid', // icon类型
                         name: 'labilitymanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
             {
@@ -70,19 +66,14 @@ const store = new Vuex.Store({
                         type: 'md-home',
                         name: 'home',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     },
                     {
                         size: 18, // icon大小
                         type: 'ios-grid', // icon类型
                         name: 'knowmanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
-
             {
                 text: '学生数据',
                 type: 'ios-paper',
@@ -92,8 +83,6 @@ const store = new Vuex.Store({
                         type: 'ios-grid',
                         name: 'studentdaoru',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }
                 ],
             },
@@ -101,35 +90,32 @@ const store = new Vuex.Store({
                 name: 'knowledgeskill', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-paper', // icon类型
-                text: '能力知识匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                text: '能力知识匹配',
             },
             {
                 name: 'radar', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-paper', // icon类型
-                text: '学生画像', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                text: '学生画像',
             },
             {
-                // name: 'jobportray', // 要跳转的路由名称 不是路径
-                // size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '人岗匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'jobportray',
+                type: 'ios-paper',
+                text: '人岗匹配',
+            },
+            {
+                name: 'resumegenerate',
+                size: 18,
+                type: 'ios-grid',
+                text: '简历生成',
                 hidden: true,
-                children: [
-                    {
-                        type: 'ios-grid',
-                        name: 'resumegenerate',
-                        size: 18,
-                        text: '简历生成',
-                    }
-                ],
             },
             {
                 name: 'teachingAssistant', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-school', // icon类型
-                text: '辅助教学', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-                hidden: true, // 隐藏此菜单项
+                text: '辅助教学',
+                hidden: true,
             },
         ],
         //管理员
@@ -138,17 +124,14 @@ const store = new Vuex.Store({
                 name: 'password', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-grid', // icon类型
-                text: '修改密码', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-
-                //    hidden: true,
-
+                text: '修改密码',
             },
             {
                 name: 'user', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-grid', // icon类型
-                text: '用户信息', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-                hidden: true, // 隐藏此菜单项
+                text: '用户信息',
+                hidden: true,
             },
             {
 
@@ -156,17 +139,17 @@ const store = new Vuex.Store({
                 type: 'ios-paper',
                 children: [
                     {
-                        name: 'schoolmanage', // 要跳转的路由名称 不是路径
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
-                        text: '学校信息管理', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-                        hidden: true, // 隐藏此菜单项
+                        name: 'schoolmanage',
+                        size: 18,
+                        type: 'ios-grid',
+                        text: '学校信息管理',
+                        hidden: true,
                     },
                     {
-                        name: 'companymanage', // 要跳转的路由名称 不是路径
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
-                        text: '企业信息管理', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                        name: 'companymanage',
+                        size: 18,
+                        type: 'ios-grid',
+                        text: '企业信息管理',
                     }],
             },
             {
@@ -177,16 +160,12 @@ const store = new Vuex.Store({
                         type: 'ios-grid',
                         name: 'labilitydaoru',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     },
                     {
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
+                        size: 18,
+                        type: 'ios-grid',
                         name: 'labilitymanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
             {
@@ -197,78 +176,66 @@ const store = new Vuex.Store({
                         type: 'md-home',
                         name: 'home',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     },
                     {
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
+                        size: 18,
+                        type: 'ios-grid',
                         name: 'knowmanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
-
             {
                 text: '学生数据',
                 type: 'ios-paper',
-                hidden: true, // 隐藏整个菜单组
+                hidden: true,
                 children: [
                     {
                         type: 'ios-grid',
                         name: 'studentdaoru',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }
                 ],
             },
             {
-                name: 'knowledgeskill', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '能力知识匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'knowledgeskill',
+                size: 18,
+                type: 'ios-paper',
+                text: '能力知识匹配',
             },
             {
-                name: 'radar', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '学生画像', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'radar',
+                size: 18,
+                type: 'ios-paper',
+                text: '学生画像',
             },
             {
-                name: 'jobportray', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '人岗匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'jobportray',
+                size: 18,
+                type: 'ios-paper',
+                text: '人岗匹配',
+            },
+            {
+                name: 'resumegenerate',
+                size: 18,
+                type: 'ios-grid',
+                text: '简历生成',
                 hidden: true,
-                children: [
-                    {
-                        name: 'resumegenerate',
-                        size: 18,
-                        type: 'ios-grid',
-                        text: '简历生成',
-                    },
-                ],
             },
             {
-                name: 'teachingAssistant', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-school', // icon类型
-                text: '辅助教学', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-                hidden: true, // 隐藏此菜单项
+                name: 'teachingAssistant',
+                size: 18,
+                type: 'ios-school',
+                text: '辅助教学',
+                hidden: true,
             },
         ],
         // 学校
         userMenuItems: [
             {
-                name: 'password', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-grid', // icon类型
-                text: '修改密码', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-
-                //    hidden: true,
-
+                name: 'password',
+                size: 18,
+                type: 'ios-grid',
+                text: '修改密码',
             },
             {
                 text: '知识体系',
@@ -278,44 +245,37 @@ const store = new Vuex.Store({
                         type: 'md-home',
                         name: 'home',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     },
                     {
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
+                        size: 18,
+                        type: 'ios-grid',
                         name: 'knowmanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
-
             {
                 text: '学生数据',
                 type: 'ios-paper',
-                hidden: true, // 隐藏整个菜单组
+                hidden: true,
                 children: [
                     {
                         type: 'ios-grid',
                         name: 'studentdaoru',
                         text: '数据导入',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     },
                 ],
             },
             {
-                name: 'knowledgeskill', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '能力知识匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'knowledgeskill',
+                size: 18,
+                type: 'ios-paper',
+                text: '能力知识匹配',
             },
             {
-                name: 'jobportray', // 要跳转的路由名称 不是路径
+                name: 'radar', // 要跳转的路由名称 不是路径
                 size: 18, // icon大小
                 type: 'ios-paper', // icon类型
-                text: '人岗匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                text: '学生画像', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
                 hidden: true,
                 children: [
                     {
@@ -327,17 +287,17 @@ const store = new Vuex.Store({
                 ],
             },
             {
-                name: 'teachingAssistant', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-school', // icon类型
-                text: '辅助教学', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-                hidden: true, // 隐藏此菜单项
+                name: 'jobportray',
+                size: 18,
+                type: 'ios-paper',
+                text: '人岗匹配',
             },
             {
-                name: 'radar', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '学生画像', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'teachingAssistant',
+                size: 18,
+                type: 'ios-school',
+                text: '辅助教学',
+                hidden: true,
             },
         ],
         // 企业
@@ -347,12 +307,10 @@ const store = new Vuex.Store({
                 type: 'ios-paper',
                 children: [
                     {
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
+                        size: 18,
+                        type: 'ios-grid',
                         name: 'labilitymanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
             {
@@ -360,48 +318,43 @@ const store = new Vuex.Store({
                 type: 'ios-paper',
                 children: [
                     {
-                        size: 18, // icon大小
-                        type: 'ios-grid', // icon类型
+                        size: 18,
+                        type: 'ios-grid',
                         name: 'knowmanage',
                         text: '体系管理',
-                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-                        // hidden: true,
                     }],
             },
             {
-                name: 'knowledgeskill', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '能力知识匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-            },
-
-            {
-                name: 'radar', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '学生画像', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'knowledgeskill',
+                size: 18,
+                type: 'ios-paper',
+                text: '能力知识匹配',
             },
             {
-                // name: 'jobportray', // 要跳转的路由名称 不是路径
-                // size: 18, // icon大小
-                type: 'ios-paper', // icon类型
-                text: '人岗匹配', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                name: 'radar',
+                size: 18,
+                type: 'ios-paper',
+                text: '学生画像',
+            },
+            {
+                name: 'jobportray',
+                size: 18,
+                type: 'ios-paper',
+                text: '人岗匹配',
+            },
+            {
+                name: 'resumegenerate',
+                size: 18,
+                type: 'ios-grid',
+                text: '简历生成',
                 hidden: true,
-                children: [
-                    {
-                        name: 'resumegenerate',
-                        size: 18,
-                        type: 'ios-grid',
-                        text: '简历生成',
-                    },
-                ],
             },
             {
-                name: 'teachingAssistant', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-school', // icon类型
-                text: '辅助教学', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
-                hidden: true, // 隐藏此菜单项
+                name: 'teachingAssistant',
+                size: 18,
+                type: 'ios-school',
+                text: '辅助教学',
+                hidden: true,
             },
         ],
         rewardDict: {
@@ -453,6 +406,3 @@ const store = new Vuex.Store({
 })
 
 export default store
-
-
-
